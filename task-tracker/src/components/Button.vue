d1d64be91f3f5961d09df1d32a31a13676b450d5
Mutesa-Cedric <mutesacedric@gmail.com>
2022-07-28 23:41:02 +0200

    feat : setup

---
+++ task-tracker/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="clickButton()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "button",
+  props: ["text", "color"],
+  methods: {
+    clickButton(){
+        console.log('clicking a button')
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
